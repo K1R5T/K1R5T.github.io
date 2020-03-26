d16ef0c57ba2775afe04aab4243dc9e71c3741a6
kirsty <kirstypurkhardt@gmail.com>
2020-03-26 18:42:31 +0000

    first commit of react portfolio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route} from "react-router-dom";
+// import logo from './logo.svg';
 import './App.css';
+// import Navbar from './components/Navbar';
+import CodeNation from './components/CodeNation';
+import AboutMe from './components/AboutMe';
+import Home from './components/Home';
+import ExternalLinks from './components/ExternalLinks';
+import Welcome from './components/Welcome'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+
+      <BrowserRouter>
+
+      {/* <Navbar /> */}
+
+      <Switch>
+
+        <Route path="/" component={Welcome} exact/>
+
+        <Route path="/home" component={Home} />
+
+        <Route path="/aboutme" component={AboutMe}/>
+
+        <Route path='/codenation' component={CodeNation}/>
+
+        <Route path='/externallinks' component={ExternalLinks}/>
+       
+
+      </Switch>
+
+      </BrowserRouter>
+
+      </div>
+    );
+  }
 }
 
 export default App;
